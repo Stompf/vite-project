734b3e1bcf0cb5f38890f96e54e2459adbf34230
Stompf <anton.lundmark@gmail.com>
2023-03-04 22:36:11 +0100

    ändrat enligt pappas design

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>WOW</h1>
+      <h1>Hej pappa!!!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
